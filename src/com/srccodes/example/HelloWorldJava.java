9b3c46061d53f5479799db3367db6d8a65377854
evris007 <papakostas@gmail.com>
2018-06-09 19:33:55 +0200

    world100

--- src/com/srccodes/example/HelloWorldJava.java
+++ src/com/srccodes/example/HelloWorldJava.java
@@ -3,7 +3,7 @@ package com.srccodes.example;
 public class HelloWorldJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World5");
+		System.out.println("Hello World100");
 
 	}
 
